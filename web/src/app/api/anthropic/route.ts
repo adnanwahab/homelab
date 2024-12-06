283cb1bce0c3583ddf30fe2cfe9d59896a046427
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-06 15:54:22 -0700

    simplify

--- web/src/app/api/anthropic/route.ts
+++ web/src/app/api/anthropic/route.ts
@@ -1,14 +1,19 @@
-{ 
-    const client = new Anthropic.Anthropic();
-    new Anthropic({ apiKey, dangerouslyAllowBrowser: true });
-    
-    const stream = await client.messages.create({
-      max_tokens: 1024,
-      messages: [{ role: 'user', content: 'Hello, Claude' }],
-      model: 'claude-3-opus-20240229',
-      stream: true,
-    });
-    for await (const messageStreamEvent of stream) {
-      console.log(messageStreamEvent.type);
-    }
-    }
+// {
+//     const client = new Anthropic.Anthropic();
+//     new Anthropic({ apiKey, dangerouslyAllowBrowser: true });
+
+//     const stream = await client.messages.create({
+//       max_tokens: 1024,
+//       messages: [{ role: 'user', content: 'Hello, Claude' }],
+//       model: 'claude-3-opus-20240229',
+//       stream: true,
+//     });
+//     for await (const messageStreamEvent of stream) {
+//       console.log(messageStreamEvent.type);
+//     }
+//     }
+//
+//
+export async function GET() {
+  return { hi: "hi" };
+}
