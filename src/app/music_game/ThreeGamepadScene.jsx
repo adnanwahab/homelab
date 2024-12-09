6ffde003d95c09b97b5df02a51f5938b5d018bfb
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-09 08:25:10 -0700

    simplify

--- src/app/music_game/ThreeGamepadScene.jsx
+++ src/app/music_game/ThreeGamepadScene.jsx
@@ -1,1 +1,10 @@
- 
+import React, { useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
+import {
+    initRendering,
+    cleanupRendering,
+    initAudio,
+    playSound
+} from './ThreeGamepadSceneHelpers.js';
+
+// Rest of your code...
