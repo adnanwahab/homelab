5a771c62d9b857979ef1d2f4978cbe51fb3d083f
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-09 10:19:48 -0700

    simplify

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,26 +1,38 @@
-//homelab admin - send this to people who pay for dynabot
 
-//download agent here - open source transparent logging - jupyter notebook literate coding.
 
-//modal, observablehq, notion,
+import setup_desktop from "./setup_desktop";
 
-// console.log("Hello via Bun!");
-// import fs from "fs";
-// import { glob } from "fast-glob";
+const index_html = `hello world
+<a href="/tools/setup_desktop">Setup Desktop 1</a>
+<a href="/tools/mailwind">Setup Desktop 2</a>
 
-// // Get all .ts files in src/agents directory
-// const agentFiles = glob.sync("src/agents/*.ts");
+<a href="/tools/mailwind">Deno WebGPU </a>
+<a href="/tools/mailwind">ollama 200,000 requests </a>
+`
 
-// // Log found agent files
-// console.log("Found agent files:", agentFiles);
+Bun.serve({
+    async fetch(req) {
+      const url = new URL(req.url);
 
-// // Import and process each agent file
-// for (const file of agentFiles) {
-//   const agentContent = fs.readFileSync(file, "utf-8");
-//   console.log(`Processing agent from ${file}:`);
-//   console.log(agentContent);
-// }
+      if (url.pathname === "/tools/setup_desktop") {
+        const result = await setup_desktop();
+        
+        return new Response(result, {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        });
+      }
 
-import getRobotLogs from "./get-robot-logs";
-import autoApplierQueue from "./auto-applier-queue";
-import UnrealPixelStreaming from "./get-robot-logs";
+      if (url.pathname === "/tools/test") {
+
+      }
+
+      return new Response(index_html, {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
+    },
+  });
+  console.log("Server is running on http://localhost:300");
