371cd0541648cc0de328387aa1fa65b7820beb0b
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-09 22:26:51 -0700

    simplify

--- web/src/components/link.tsx
+++ web/src/components/link.tsx
@@ -1,11 +1,14 @@
 import * as Headless from '@headlessui/react'
-import NextLink from 'next/link'
-import { forwardRef } from 'react'
+import NextLink, { type LinkProps } from 'next/link'
+import React, { forwardRef } from 'react'
 
-export const Link = forwardRef(function Link(props, ref) {
+export const Link = forwardRef(function Link(
+  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
+  ref: React.ForwardedRef<HTMLAnchorElement>
+) {
   return (
     <Headless.DataInteractive>
-      <NextLink ref={ref} {...props} />
+      <NextLink {...props} ref={ref} />
     </Headless.DataInteractive>
   )
 })
