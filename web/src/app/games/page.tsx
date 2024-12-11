207aef54a65c7cbc86ca2f0b925c819e55b20080
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-11 12:12:56 -0700

    simplify

--- web/src/app/games/page.tsx
+++ web/src/app/games/page.tsx
@@ -1,4 +1,36 @@
 'use client'
+const Games = [
+    {
+        "name": "Music Game",
+        "imgsrc": "https://media4.giphy.com/media/3otPoo8NDLOmzvTJF6/giphy.gif?cid=6c09b9524cu16ebo4tsyoifh0dxzoiho2unj1os0ytb9wxf1&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+        "description": "Create and play with interactive music visualizations",
+        "type": "music_game",
+        "link": "/games/music_game"
+    },
+    {
+        "name": "Physics Playground",
+        "imgsrc": "https://picsum.photos/200/300",
+        "description": "Experiment with physics-based interactions and puzzles",
+        "type": "physics_game",
+        "link": "/games/physics_game"
+    },
+    {
+        "name": "Puzzle Challenge",
+        "imgsrc": "https://picsum.photos/200/300",
+        "description": "Solve mind-bending puzzles and challenges",
+        "type": "puzzle_game",
+        "link": "/games/puzzle_game"
+    },
+    {
+        "name": "Dwarf Game",
+        "imgsrc": "https://picsum.photos/200/300",
+        "description": "A dwarf fortress inspired simulation game",
+        "type": "simulation_game",
+        "link": "/games/dwarf_game"
+    }
+]
+
+
 import { Tldraw } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 //import App from './edit/page'
@@ -18,8 +50,6 @@ import Link from 'next/link'
 
 ///we want 3 to 15 possibiles - scenario- rendered 
 
-const level_list = import('./games.json');
-
 function LevelsGrid() {
     return (
         <div style={{ padding: '20px' }}>
@@ -29,7 +59,7 @@ function LevelsGrid() {
                 gap: '20px',
                 width: '100%'
             }}>
-                {level_list.map((item, index) => (
+                {games.map((item, index) => (
                     <div key={index} style={{
                         border: '1px solid #e0e0e0',
                         borderRadius: '8px',
