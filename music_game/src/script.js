04628658c9931239dc96fd6c69693e529ed6a0d1
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-12 10:04:37 -0700

    simplify

---
+++ music_game/src/script.js
@@ -0,0 +1,13 @@
+import Game from './Game.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import gsap from 'gsap';
+import * as THREE from 'three';
+
+// Instantiate the game
+//const game = new Game();
+
+
+import jolt from './jolt.js';
+
+jolt();
