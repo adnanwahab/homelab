223be64bf27b36c25a539da24151390c610f09a8
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-12 16:27:35 -0700

    simplify

--- music_game/src/script.js
+++ music_game/src/script.js
@@ -1,54 +1,50 @@
+import Game from "./games/mixed_mode.js";
+import GamePhysics from "./games/physics.js";
+import Multiplex from "./utils/Multiplex.js";
+import auto_debugger from "./utils/auto_debugger.ts";
+import initializeWebSocket from "./utils/websocket.js";
+import music_game from "./games/music_game.js";
+import wiki_game from "./utils/wiki-game.ts";
+import platformer from "./games/platformer.js";
 
-import Game from './games/mixed_mode.js';
-import GamePhysics from './games/physics.js';
-import Multiplex from './utils/Multiplex.js';
-import auto_debugger from './utils/auto_debugger.ts';
-import initializeWebSocket from './utils/websocket.js';
-import music_game from './games/music_game.js';
-import wiki_game from './utils/wiki-game.ts';
-import platformer from './games/platformer.js';
-
-//utils 
+//utils
 auto_debugger();
 //initializeWebSocket();
 //games
 // const game = new Game(document.querySelector('.canvas-red'));
 // new game_music(document.querySelector('.canvas-blue'));
 
+import { label as label_one } from "./games/platformer.js";
 
+document.getElementById("label-one").innerHTML = label_one;
 
+import { label as label_two } from "./games/music_game.js";
 
+document.getElementById("label-two").innerHTML = label_two;
 
-import {label as label_one} from './games/platformer.js';
+import { label as label_three } from "./games/physics.js";
+document.getElementById("label-three").innerHTML = label_three;
 
-document.getElementById('label-one').innerHTML = label_one;
-
-import {label as label_two} from './games/music_game.js';
-
-document.getElementById('label-two').innerHTML = label_two;
-
-import {label as label_three} from './games/physics.js';
-document.getElementById('label-three').innerHTML = label_three;
-
-
-
-platformer(document.querySelector('.canvas-red'));
-new music_game(document.querySelector('.canvas-blue'));
-GamePhysics(document.querySelector('.canvas-green'), [
+platformer(document.querySelector(".canvas-red"));
+new music_game(document.querySelector(".canvas-blue"));
+GamePhysics(
+  document.querySelector(".canvas-green"),
+  [
     {
-        type: 'box',
-        position: [0, 0, 0],
-        size: [1, 1, 1]
-    }
-], function (add_object) {
-    console.log('done');
+      type: "box",
+      position: [0, 0, 0],
+      size: [1, 1, 1],
+    },
+  ],
+  function (add_object) {
+    console.log("done");
     setInterval(() => {
-        console.log('done');
-        add_object();
+      console.log("done");
+      add_object();
     }, 1000);
-    window.addEventListener('mousemove', add_object);   
-});
-
+    window.addEventListener("mousemove", add_object);
+  },
+);
 
 // drawing(document.querySelector('.canvas-purple'));
 
@@ -56,13 +52,11 @@ GamePhysics(document.querySelector('.canvas-green'), [
 
 //Multiplex(document.querySelector('.multiplex-canvas'));
 
-
-import Games_collection from './utils/games_collection.json';
+import games_collection from "./utils/games_collection.json";
 
 //console.log(Games_collection);
 
-
-import games_list_view from './utils/games_list_view.js';
+import games_list_view from "./utils/games_list_view.js";
 
 //src/utils/games_list_view.js
 //games_list_view();
@@ -71,7 +65,6 @@ import games_list_view from './utils/games_list_view.js';
 // gameContainer.addEventListener('mouseenter', (e) => {
 //     console.log(e.target.id);
 // });
-
 
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
