4686c91cefbfe2501d33eb3184232aec432901bc
Adnan Wahab <shel.bernstein123@gmail.com>
2024-12-11 17:42:11 -0700

    simplify

--- myReactNativeExpoApp/app/MusicGame.tsx
+++
@@ -1,9 +0,0 @@
-function MusicGame() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the Profile Screen.</Text>
-    </View>
-  );
-}
-
-export default MusicGame;
