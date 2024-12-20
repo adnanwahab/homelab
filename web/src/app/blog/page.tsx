b238496c5e8c82723b35b299fda0f75a0be2b824
pau <shel.bernstein123@gmail.com>
2024-12-19 20:20:37 -0700

    simplify

--- web/src/app/blog/page.tsx
+++ web/src/app/blog/page.tsx
@@ -16,6 +16,9 @@ export default function () {
           src="https://observablehq.com/embed/@roboticsuniversity/simulation?cells=ezgifComOptimize13&banner=false"
         ></iframe>
       </div>
+      <iframe src="https://blogs.nvidia.com/blog/nvidia-zoox-autonomous-ride-hailing/" />
+      <div>court room of the future</div>
+      <div>https://www.tiktok.com/@nechamanikky/video/7205957396798278958</div>
       <div></div>
       <div>
         convert reasoning model from eletrons in neurons to atom-based structure
@@ -196,81 +199,81 @@ const steps = [
   { name: "Step 5", href: "#", status: "upcoming" },
 ];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(" ");
+// }
 
-function Timeline() {
-  return (
-    <nav aria-label="Progress">
-      <ol role="list" className="flex items-center">
-        {steps.map((step, stepIdx) => (
-          <li
-            key={step.name}
-            className={classNames(
-              stepIdx !== steps.length - 1 ? "pr-8 sm:pr-20" : "",
-              "relative",
-            )}
-          >
-            {step.status === "complete" ? (
-              <>
-                <div
-                  aria-hidden="true"
-                  className="absolute inset-0 flex items-center"
-                >
-                  <div className="h-0.5 w-full bg-indigo-600" />
-                </div>
-                <a
-                  href="#"
-                  className="relative flex size-8 items-center justify-center rounded-full bg-indigo-600 hover:bg-indigo-900"
-                >
-                  <CheckIcon aria-hidden="true" className="size-5 text-white" />
-                  <span className="sr-only">{step.name}</span>
-                </a>
-              </>
-            ) : step.status === "current" ? (
-              <>
-                <div
-                  aria-hidden="true"
-                  className="absolute inset-0 flex items-center"
-                >
-                  <div className="h-0.5 w-full bg-gray-200" />
-                </div>
-                <a
-                  href="#"
-                  aria-current="step"
-                  className="relative flex size-8 items-center justify-center rounded-full border-2 border-indigo-600 bg-white"
-                >
-                  <span
-                    aria-hidden="true"
-                    className="size-2.5 rounded-full bg-indigo-600"
-                  />
-                  <span className="sr-only">{step.name}</span>
-                </a>
-              </>
-            ) : (
-              <>
-                <div
-                  aria-hidden="true"
-                  className="absolute inset-0 flex items-center"
-                >
-                  <div className="h-0.5 w-full bg-gray-200" />
-                </div>
-                <a
-                  href="#"
-                  className="group relative flex size-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white hover:border-gray-400"
-                >
-                  <span
-                    aria-hidden="true"
-                    className="size-2.5 rounded-full bg-transparent group-hover:bg-gray-300"
-                  />
-                  <span className="sr-only">{step.name}</span>
-                </a>
-              </>
-            )}
-          </li>
-        ))}
-      </ol>
-    </nav>
-  );
-}
+// function Timeline() {
+//   return (
+//     <nav aria-label="Progress">
+//       <ol role="list" className="flex items-center">
+//         {steps.map((step, stepIdx) => (
+//           <li
+//             key={step.name}
+//             className={classNames(
+//               stepIdx !== steps.length - 1 ? "pr-8 sm:pr-20" : "",
+//               "relative",
+//             )}
+//           >
+//             {step.status === "complete" ? (
+//               <>
+//                 <div
+//                   aria-hidden="true"
+//                   className="absolute inset-0 flex items-center"
+//                 >
+//                   <div className="h-0.5 w-full bg-indigo-600" />
+//                 </div>
+//                 <a
+//                   href="#"
+//                   className="relative flex size-8 items-center justify-center rounded-full bg-indigo-600 hover:bg-indigo-900"
+//                 >
+//                   <CheckIcon aria-hidden="true" className="size-5 text-white" />
+//                   <span className="sr-only">{step.name}</span>
+//                 </a>
+//               </>
+//             ) : step.status === "current" ? (
+//               <>
+//                 <div
+//                   aria-hidden="true"
+//                   className="absolute inset-0 flex items-center"
+//                 >
+//                   <div className="h-0.5 w-full bg-gray-200" />
+//                 </div>
+//                 <a
+//                   href="#"
+//                   aria-current="step"
+//                   className="relative flex size-8 items-center justify-center rounded-full border-2 border-indigo-600 bg-white"
+//                 >
+//                   <span
+//                     aria-hidden="true"
+//                     className="size-2.5 rounded-full bg-indigo-600"
+//                   />
+//                   <span className="sr-only">{step.name}</span>
+//                 </a>
+//               </>
+//             ) : (
+//               <>
+//                 <div
+//                   aria-hidden="true"
+//                   className="absolute inset-0 flex items-center"
+//                 >
+//                   <div className="h-0.5 w-full bg-gray-200" />
+//                 </div>
+//                 <a
+//                   href="#"
+//                   className="group relative flex size-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white hover:border-gray-400"
+//                 >
+//                   <span
+//                     aria-hidden="true"
+//                     className="size-2.5 rounded-full bg-transparent group-hover:bg-gray-300"
+//                   />
+//                   <span className="sr-only">{step.name}</span>
+//                 </a>
+//               </>
+//             )}
+//           </li>
+//         ))}
+//       </ol>
+//     </nav>
+//   );
+// }
