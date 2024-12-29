ec88595e7a65d6eecf1c431c80b9ae751011b01b
pau <shel.bernstein123@gmail.com>
2024-12-29 05:46:37 -0600

    simplify

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+
+        <SpeedInsights />
+      </body>
+    </html>
+  )
+}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
