109cbe4c603e4de752257e970c8d345e16a3db69
pau <shel.bernstein123@gmail.com>
2024-12-31 10:40:27 -0600

    simplify

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   process.env.SUPABASE_URL || "https://ewkxvbgbzikwwudriebh.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY || "your-key-here";
+const supabaseKey = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3a3h2Ymdiemlrd3d1ZHJpZWJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MjYzMDEsImV4cCI6MjA0OTEwMjMwMX0.LjW3EYwo7bg4Za_fGjXKGXS92fPqcAGVJRrbr3Vgh0Y";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const getAllKV = async () => {
