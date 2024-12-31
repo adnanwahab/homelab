8bc31373307547928238af6078fce6488f547036
pau <shel.bernstein123@gmail.com>
2024-12-31 12:11:47 -0600

    simplify

--- web/src/app/simulation_editor/page.tsx
+++ web/src/app/simulation_editor/page.tsx
@@ -1,7 +1,22 @@
 export default function SimulationEditor() {
-  return <div>Simulation Editor
-
-
-    <iframe src="https://taiyongrobotics.com" width="100%" height="100%"></iframe>
-  </div>;
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100vw',
+      height: '100vh',
+      margin: 0,
+      padding: 0,
+    }}>
+      <iframe 
+        src="https://taiyongrobotics.com" 
+        width="100%" 
+        height="100%" 
+        style={{
+          border: 'none',
+        }}
+      />
+    </div>
+  );
 }
