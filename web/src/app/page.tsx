bb05702ed85a3b203c019537ea2bd5f7301a3b5d
pau <shel.bernstein123@gmail.com>
2024-12-31 13:09:08 -0600

    simplify

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -388,6 +388,8 @@ function Header() {
   )
 }
 
+const completed = 10
+
 function PortfolioGrid() {
   const [shuffledFiles, setShuffledFiles] = useState<FileItem[]>(files);
 
@@ -408,7 +410,7 @@ function PortfolioGrid() {
       <h2 className="text-2xl font-semibold mb-6 text-[#8b98e8]">Visualisations</h2>
       <ul role="list" className="grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-3 lg:grid-cols-4">
         {shuffledFiles.map((file, index) => (
-          <Link href={``} key={index}>
+          <Link href={ completed > index ? `/demos/${index}` : `/demos/${index}`} key={index}>
             <li className="relative group">
               <div className="aspect-[10/7] overflow-hidden rounded-lg bg-gray-800">
                 <img
