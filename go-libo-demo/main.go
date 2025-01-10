69d651af3aed30bc33875e6d8676a55452a34d10
adnan wahab <mail@adnanwahab.com>
2025-01-10 17:30:48 -0600

    simplify

---
+++ go-libo-demo/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/qedus/osmpbf"
+)
+
+func main() {
+
+	f, err := os.Open("/home/adnan/planet-241230.osm.pbf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	d := osmpbf.NewDecoder(f)
+
+	// use more memory from the start, it is faster
+	d.SetBufferSize(osmpbf.MaxBlobSize)
+
+	// start decoding with several goroutines, it is faster
+	err = d.Start(runtime.GOMAXPROCS(-1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var nc, wc, rc uint64
+	for {
+		fmt.Printf("Nodes: %d, Ways: %d, Relations")
+		if v, err := d.Decode(); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			switch v := v.(type) {
+			case *osmpbf.Node:
+				// Process Node v.
+				nc++
+			case *osmpbf.Way:
+				// Process Way v.
+				wc++
+			case *osmpbf.Relation:
+				// Process Relation v.
+				rc++
+			default:
+				log.Fatalf("unknown type %T\n", v)
+			}
+		}
+	}
+
+	fmt.Printf("Nodes: %d, Ways: %d, Relations: %d\n", nc, wc, rc)
+}
