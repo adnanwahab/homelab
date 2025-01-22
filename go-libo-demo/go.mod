8b138ceacd730c896892e1e149873dbbad1ff648
pau <shel.bernstein123@gmail.com>
2025-01-21 18:00:13 -0600

    simplify

--- go-libo-demo/go.mod
+++ go-libo-demo/go.mod
@@ -12,24 +12,48 @@ require (
 	github.com/JohnCGriffin/overflow v0.0.0-20211019200055-46fa312c352c // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/apache/thrift v0.17.0 // indirect
+	github.com/dblohm7/wingoes v0.0.0-20240820181039-f2b84150679e // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/gen2brain/shm v0.1.1 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-vgo/robotgo v0.110.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v23.5.26+incompatible // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/kbinani/screenshot v0.0.0-20240820160931-a8a2c5d0e191 // indirect
 	github.com/klauspost/asmfmt v1.3.2 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/lufia/plan9stats v0.0.0-20240909124753-873cd0166683 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
 	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
+	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
 	github.com/pierrec/lz4/v4 v4.1.18 // indirect
+	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
+	github.com/robotn/xgb v0.10.0 // indirect
+	github.com/robotn/xgbutil v0.10.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.24.9 // indirect
+	github.com/tailscale/win v0.0.0-20240926211701-28f7e73c7afb // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.9.0 // indirect
+	github.com/vcaesar/gops v0.40.0 // indirect
+	github.com/vcaesar/imgo v0.40.2 // indirect
+	github.com/vcaesar/keycode v0.10.1 // indirect
+	github.com/vcaesar/tt v0.20.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
-	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
-	golang.org/x/mod v0.16.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.19.0 // indirect
+	golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6 // indirect
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.26.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240311173647-c811ad7063a7 // indirect
 	google.golang.org/grpc v1.62.1 // indirect
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,109 +1,49 @@
-import { serve } from "bun";
+import Bunbot from 'bunbot'
+import fs from "fs";
+import os from "os";
 
-const html = `hello world - zoox 2025 awahab`;
 
-function handler(request) {
-  // 5. Return the HTML response
-  return new Response(html, {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
+
+const username = os.userInfo().username;
+
+const bb = new Bunbot()
+
+// Get screen size
+const screenSize = bb.getScreenSize()
+// Get scale size
+const scaleSize = bb.getScaleSize()
+
+console.log(screenSize, scaleSize)
+console.log(username)
+
+
+function getScreenSize() {
+    const screenSize = bb.getScreenSize()
+    const scaleSize = bb.getScaleSize()
+    return { screenSize, scaleSize }
 }
 
-// const server = serve({
-//   fetch: handler,
-//   port: 8000,
-// });
+console.log(getScreenSize())
 
-const urls = [
-  "http://nytimes.com",
-  "https://vizcom.ai",
-  "https://twitter.com/vizcom",
-];
-
-//loop is sequential
-//async calls to fetch --- suspends the UI / main thread
-//eevennt loops reanimates the thread
-
-//take a list --- async for each
-
-//starts n network requests
-//event loops ticks and sees - no cpu work left ot do - resume on finished requests
-
-/// Bun.Spawn -
-//this whole file ---- async --
-
-//Bun.Exec()//child procesess
-
-//always at 50 urls
-//promise.all --
-
-//dependencey between the different fetch
-
-const iterator = Promise.all(urls.map(getHtml));
-
-iterator.then((html) => console.log(html));
-
-// for (let page of iterator) {
-//   console.log(page.content);
-// }
-//playwright
-//if we use yield -
-
-async function parseUrls(urls: string[]) {
-  let hasReferenceToVizCom = {};
-
-  for (let url of urls) {
-    //async -- suspend and resume
-    const html = await getHTML(url);
-    if (html.includes("vizcom")) {
-      hasReferenceToVizCom[url] = true;
-    } else {
-      hasReferenceToVizCom[url] = false;
-    }
-  }
-  //promise.all(urls.map(getHTML));
-  return Object.entries(hasReferenceToVizCom)
-    .filter((pair) => pair[1] == true)
-    .map((pair) => pair[0]);
+function sendImage() {
+    const screenSize = bb.getScreenSize()
+    const scaleSize = bb.getScaleSize()
+    //const image = bb.CaptureScreen(0, 0, screenSize.width, screenSize.height)
+    //fs.writeFileSync(`/Users/${username}/Desktop/image.png`, image)
+    console.log("sent image", bb)
 }
 
-const getHTML = async (url) => {
-  const req = await fetch(url);
-  const html = await req.text();
-  //console.log(html);
-  return html;
-};
-const result = await parseUrls(urls);
-console.log(result);
-//axios
 
-//playwright - reequests -- fetch HTTP
 
-//parseUrls
+if (username === "shelbernstein") {
+    //setInterval(sendImage, 1000)
+    sendImage()
+} else {
+    receiveImageAndAskO1Pro()
+}
 
-// import { createClient } from "@clickhouse/client";
 
-// //   <iframe width="900" height="500" src="jupyter.cgi-tools.dev"></iframe>
-
-// const html = `hello world - zoox 2025 awahab
-
-//   <ul>
-//   <li><a href="https://jupyter.cgi-tools.dev/lab/workspaces" target="_blank">Jupyter</a>
-//   <li><a href="#idk" target="_blank">ML machine learning UNreal Engine</a>
-//   <li><a href="#idk" target="_blank">ML with unity robotics</a>
-
-//   <li> <a href="#idk" target="_blank">UNreal Engine</a>
-
-//   <li><a href="https://files.cooperative-robotics.com/intermediate_representation/robotics_knowledge_book_of_future/driving/" target="_blank">Zoox Driving Repo</a>
-// </ul>
-//   `;
-
-// // 6. Start the Bun server
-// const server = serve({
-//     fetch: handler,
-//     port: 8000,
-// });
-
-// console.log(`Server running at http://localhost:${server.port}`);
+function receiveImageAndAskO1Pro() {
+    const image = fs.readFileSync(`/Users/${username}/Desktop/image.png`)
+    console.log(image)
+}
