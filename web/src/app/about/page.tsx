15addf635be89a6ee22dce43f3dd8b78e8a5b5bc
pau <shel.bernstein123@gmail.com>
2025-01-22 09:59:36 -0600

    simplify

---
+++ web/src/app/about/page.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return <div>About
+
+
+              <a
+            href="mailto:adnan@llama-tools.com"
+            className="hover:text-[#8b98e8]"
+          >
+            email
+          </a> 
+   <a
+            href="https://github.com/adnanwahab"
+            className="hover:text-[#8b98e8]"
+          >
+            github
+          </a> 
+    <a href="https://x.com/adnan_wahab_" className="hover:text-[#8b98e8]">
+            twitter
+          </a>
+        <a
+            href="https://linkedin.com/in/dynabot"
+            className="hover:text-[#8b98e8]"
+          >
+            linkedin
+          </a>
+  </div>
+}
