2cacb28aad0916f6dcc8ab3fa5c3d64772a1a616
pau <shel.bernstein123@gmail.com>
2025-01-28 05:00:07 -0600

    simplify

---
+++ web/src/app/demos/6/page.tsx
@@ -0,0 +1,4 @@
+export default function Demo6Page() {
+  return <div><iframe width="100%" height="500" 
+  src="https://observablehq.com/embed/e2dc1404de6b630d?cell=*&banner=false"></iframe></div>;
+}
