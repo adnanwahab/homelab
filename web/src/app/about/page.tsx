041c0e0a00a1d68ed00651aa9525d8e7e4ecbcd0
pau <shel.bernstein123@gmail.com>
2025-02-11 00:28:07 -0600

    simplify

--- web/src/app/about/page.tsx
+++ web/src/app/about/page.tsx
@@ -1,27 +1,45 @@
-export default function About() {
-  return <div>About
-
-
-              <a
-            href="mailto:adnan@llama-tools.com"
-            className="hover:text-[#8b98e8]"
-          >
-            email
-          </a> 
-   <a
-            href="https://github.com/adnanwahab"
-            className="hover:text-[#8b98e8]"
-          >
-            github
-          </a> 
-    <a href="https://x.com/adnan_wahab_" className="hover:text-[#8b98e8]">
-            twitter
-          </a>
-        <a
-            href="https://linkedin.com/in/dynabot"
-            className="hover:text-[#8b98e8]"
-          >
-            linkedin
-          </a>
+export default function () {
+<div className="flex flex-col items-center p-7 rounded-2xl">
+  <div>
+    <img className="size-48 shadow-xl rounded-md" alt="" src="/img/cover.png" />
   </div>
+  <div className="flex">
+    <span>Class Warfare</span>
+    <span>The Anti-Patterns</span>
+    <span className="flex">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+
 }
+
+// export default function About() {
+//   return <div>About
+
+
+//               <a
+//             href="mailto:adnan@llama-tools.com"
+//             className="hover:text-[#8b98e8]"
+//           >
+//             email
+//           </a> 
+//    <a
+//             href="https://github.com/adnanwahab"
+//             className="hover:text-[#8b98e8]"
+//           >
+//             github
+//           </a> 
+//     <a href="https://x.com/adnan_wahab_" className="hover:text-[#8b98e8]">
+//             twitter
+//           </a>
+//         <a
+//             href="https://linkedin.com/in/dynabot"
+//             className="hover:text-[#8b98e8]"
+//           >
+//             linkedin
+//           </a>
+//   </div>
+// }
