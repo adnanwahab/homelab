2e4a7d2f689dce7762caad6e20f266df1e48a7ae
pau <shel.bernstein123@gmail.com>
2025-02-11 08:10:34 -0600

    simplify

--- web/src/app/books/page.tsx
+++ web/src/app/books/page.tsx
@@ -1,4 +1,11 @@
 //1 book per daay from 2025 till 2052
 export default function () {
-    return <></>
+    return <>
+    
+    
+    <h1>Coming soon </h1>
+    <h2>I'm trying to read one book per day for the next 27 years from 2024-2049</h2>
+
+    
+    </>
 }
