567902edbf0720134a8e9c888e25693a736c22d6
adnan wahab <mail@adnanwahab.com>
2025-04-02 14:17:04 -0500

    simplify

---
+++ web/src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+export default function () {
+    return <>
+    <ul>
+        <li>WebRTC Steam Streaming</li>
+
+        <li>Auto-magic Book_reader</li>
+
+        <li>Replay Analyzer</li>
+
+    </ul>
+    
+    </>
+}
