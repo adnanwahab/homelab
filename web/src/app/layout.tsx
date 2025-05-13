c654414162a748f386dc8373a304aba97f3e0c23
pau <shel.bernstein123@gmail.com>
2025-05-13 18:42:42 -0500

    simplify

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,34 +1,28 @@
-
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Script from 'next/script';
-
-
+//import { SpeedInsights } from '@vercel/speed-insights/next'
+import Script from 'next/script'
 
 //import { WebGPUProvider } from './context/WebGPUContext';
 
-
-
-import WebGPULayout from './WebGPULayout';
-
+import WebGPULayout from './WebGPULayout'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Adnan Wahab",
-  description: "Adnan Wahab - ThreeJS + ML + Devops Engineer",
-};
+  title: 'Adnan Wahab',
+  description: 'Adnan Wahab - ThreeJS + ML + Devops Engineer',
+}
 
 export default function RootLayout({
   children,
@@ -37,20 +31,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      <WebGPULayout>
-        {children}
-        <Analytics />
-        <Script id="microsoft-clarity">
-          {`
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <WebGPULayout>
+          {children}
+          <Analytics />
+          <Script id="microsoft-clarity">
+            {`
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "ok8h7h9e0q");
           `}
-        </Script>
-        <SpeedInsights />
+          </Script>
+          {/* <SpeedInsights /> */}
         </WebGPULayout>
       </body>
     </html>
