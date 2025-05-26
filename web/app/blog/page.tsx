daf4a68117736f9e3bebc8845ce3d604ac1f2524
pau <shel.bernstein123@gmail.com>
2025-05-26 11:23:07 -0500

    simplify

--- web/app/blog/page.tsx
+++ web/app/blog/page.tsx
@@ -1,14 +1,16 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: "Adnan Wahab's Blog",
+  description: 'Read my blog about ML, culture and civilization.',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        Adnan Wahab's Blog
+      </h1>
       <BlogPosts />
     </section>
   )
