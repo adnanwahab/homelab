b28b4bc719b1c897160ee3bd36749c2a4ed846f2
pau <shel.bernstein123@gmail.com>
2025-05-26 12:42:47 -0500

    simplify

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-       Adnan Wahab
+       Adnan Wahab 
       </h1>
       <p className="mb-4">
-
+        Under construction
       </p>
       <div className="my-8">
         <BlogPosts />
