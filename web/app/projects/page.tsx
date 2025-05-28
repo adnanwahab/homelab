1729ee8bdab708460b84d5b2acd829eb5cae6fa8
pau <shel.bernstein123@gmail.com>
2025-05-28 16:05:08 -0500

    simplify

---
+++ web/app/projects/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <div>
+      <ul>
+                <li>
+                <a href="/projects/replay_analyzer">Replay Analyzer</a>
+                </li>
+        <li>
+          <a href="/projects/karaoke">karaoke</a>
+        </li>
+        <li>
+          <a href="/projects/voice_coach">voice coach</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
