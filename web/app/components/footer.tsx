7b0f05065efb912cab1b04258ed4c119893465ea
pau <shel.bernstein123@gmail.com>
2025-06-17 11:13:05 -0500

    simplify

--- web/app/components/footer.tsx
+++ web/app/components/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://twitter.com/adnanwahab"
+            href="https://twitter.com/adnan_wahab_"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">twitter</p>
